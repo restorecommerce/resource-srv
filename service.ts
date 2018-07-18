da279ddf419a214a64ae587b66961d1047783578
akumar <arun.kumar@n-fuse.co>
2018-07-18 15:56:38 +0200

    up for graph srv.

--- service.ts
+++ service.ts
@@ -8,7 +8,7 @@ import {
   grpc, Server, OffsetStore
 } from '@restorecommerce/chassis-srv';
 import { Events, Topic } from '@restorecommerce/kafka-client';
-import { ResourcesAPIBase, ServiceBase } from '@restorecommerce/resource-base-interface';
+import { ResourcesAPIBase, ServiceBase, GraphResourcesServiceBase } from '@restorecommerce/resource-base-interface';
 
 export class Worker {
   server: Server;
@@ -86,7 +86,8 @@ export class Worker {
 
     const logger = new Logger(cfg.get('logger'));
     const server = new Server(cfg.get('server'), logger);
-    const db = await co(database.get(cfg.get('database:arango'), logger));
+    const db = await co(database.get(cfg.get('database:arango'),
+      logger, cfg.get('graph:graphName')));
     const events = new Events(cfg.get('events:kafka'), logger);
 
     await events.start();
@@ -103,6 +104,7 @@ export class Worker {
 
     // Enable events firing for resource api using config
     const isEventsEnabled = (cfg.get('events:enableCRUDEvents') == 'true');
+    const graphCfg = cfg.get('graph');
 
     for (let resourceType in resources) {
       const resourceCfg = resources[resourceType];
@@ -130,7 +132,17 @@ export class Worker {
         }
         logger.info(`Setting up ${resourceName} resource service`);
 
-        const resourceAPI = new ResourcesAPIBase(db, `${resourceName}s`, resourceFieldConfig);
+        let edgeCfg;
+        let graphName;
+        if (graphCfg && graphCfg.vertices) {
+          const collectionName = `${resourceName}s`;
+          edgeCfg = graphCfg.vertices[collectionName];
+        }
+        if (graphCfg) {
+          graphName = graphCfg.graphName;
+        }
+        const resourceAPI = new ResourcesAPIBase(db, `${resourceName}s`,
+          resourceFieldConfig, edgeCfg, graphName);
         const resourceEvents = events.topic(`${resourcesServiceNamePrefix}${resourceName}s.resource`);
         const service = new ServiceBase(resourceName,
           resourceEvents, logger, resourceAPI, isEventsEnabled);
@@ -139,7 +151,7 @@ export class Worker {
     }
 
     // Add CommandInterfaceService
-    const cis: ICommandInterface = new CommandInterface(server, cfg.get(), logger, events);
+    const cis: ICommandInterface = new ResourceCommandInterface(server, cfg.get(), logger, events);
     const cisName = cfg.get('command-interface:name');
     await server.bind(cisName, cis);
 
@@ -163,7 +175,7 @@ export class Worker {
       if (kafkaCfg.topics[topicType].events) {
         const eventNames = kafkaCfg.topics[topicType].events;
         for (let eventName of eventNames) {
-          await topic.on(eventName, resourcesServiceEventListener, offSetValue);
+          await topic.on(eventName, resourcesServiceEventListener, { startingOffset: offSetValue });
         }
       }
     }
@@ -173,6 +185,10 @@ export class Worker {
     const transport = server.transport[transportName];
     const reflectionService = new grpc.ServerReflection(transport.$builder, server.config);
     await server.bind('reflection', reflectionService);
+
+    // graph Service
+    const graphAPIService = new GraphResourcesServiceBase(db, cfg.get('fieldHandlers:bufferFields'));
+    await server.bind('graph', graphAPIService);
 
     // Start server
     await server.start();
@@ -194,6 +210,148 @@ export class Worker {
     if (this.redisClient) {
       await this.redisClient.quit();
     }
+  }
+}
+
+class ResourceCommandInterface extends CommandInterface {
+  edgeCfg: any;
+  // graphName: any;
+  constructor(server: Server, cfg: any, logger: any, events: Events) {
+    super(server, cfg, logger, events);
+    if (cfg.graph && cfg.graph.vertices) {
+      this.edgeCfg = cfg.graph.vertices;
+      // this.graphName = cfg.graph.graphName;
+    }
+  }
+
+  makeResourcesRestoreSetup(db: any, resource: string): any {
+    const that = this;
+    const collectionName = `${resource}s`;
+    return {
+      [`${resource}Created`]: async function restoreCreated(message: any,
+        context: any, config: any, eventName: string, done: any): Promise<any> {
+        that.decodeBufferField(message, resource);
+        try {
+          if (that.edgeCfg[collectionName]) {
+            const result = await db.findByID(collectionName, message.id);
+            if (result.length > 0) {
+              done();
+              return {};
+            }
+            await db.createVertex(collectionName, message);
+            // Based on graphCfg create the necessary edges
+            for (let eachEdgeCfg of that.edgeCfg[collectionName]) {
+              const fromIDkey = eachEdgeCfg.from;
+              const from_id = message[fromIDkey];
+              const toIDkey = eachEdgeCfg.to;
+              const to_id = message[toIDkey];
+              const fromVerticeName = collectionName;
+              const toVerticeName = eachEdgeCfg.toVerticeName;
+              if (fromVerticeName && toVerticeName) {
+                const edgeDefRes = await db.addEdgeDefinition(eachEdgeCfg.edgeName, [fromVerticeName],
+                  [toVerticeName]);
+              }
+              if (from_id && to_id) {
+                if (_.isArray(to_id)) {
+                  for (let toID of to_id) {
+                    await db.createEdge(eachEdgeCfg.edgeName, null,
+                      `${fromVerticeName}/${from_id}`, `${toVerticeName}/${toID}`);
+                  }
+                  continue;
+                }
+                await db.createEdge(eachEdgeCfg.edgeName, null,
+                  `${fromVerticeName}/${from_id}`, `${toVerticeName}/${to_id}`);
+              }
+            }
+          } else {
+            await db.insert(collectionName, message);
+          }
+        } catch (err) {
+          if (done) {
+            done();
+          }
+        }
+        if (done) {
+          done();
+        }
+        return {};
+      },
+      [`${resource}Modified`]: async function restoreModified(message: any,
+        context: any, config: any, eventName: string, done: any): Promise<any> {
+        that.decodeBufferField(message, resource);
+        // Based on graphcfg update necessary edges
+        try {
+          if (that.edgeCfg[collectionName]) {
+            const foundDocs = await db.find(collectionName, { id: message.id });
+            const dbDoc = foundDocs[0];
+            for (let eachEdgeCfg of that.edgeCfg[collectionName]) {
+              const toIDkey = eachEdgeCfg.to;
+              let modified_to_idValues = message[toIDkey];
+              let db_to_idValues = dbDoc[toIDkey];
+              if (_.isArray(modified_to_idValues)) {
+                modified_to_idValues = _.sortBy(modified_to_idValues);
+              }
+              if (_.isArray(db_to_idValues)) {
+                db_to_idValues = _.sortBy(db_to_idValues);
+              }
+              // delete and recreate only if there is a difference in references
+              if (!_.isEqual(modified_to_idValues, db_to_idValues)) {
+                const fromIDkey = eachEdgeCfg.from;
+                const from_id = message[fromIDkey];
+                const fromVerticeName = collectionName;
+                const toVerticeName = eachEdgeCfg.toVerticeName;
+
+                const edgeCollectionName = eachEdgeCfg.edgeName;
+                let outgoingEdges: any = await db.getOutEdges(edgeCollectionName, `${collectionName}/${dbDoc.id}`);
+                for (let outgoingEdge of outgoingEdges) {
+                  const removedEdge = await db.removeEdge(edgeCollectionName, outgoingEdge._id);
+                }
+                // Create new edges
+                if (from_id && modified_to_idValues) {
+                  if (_.isArray(modified_to_idValues)) {
+                    for (let toID of modified_to_idValues) {
+                      await db.createEdge(eachEdgeCfg.edgeName, null,
+                        `${fromVerticeName}/${from_id}`, `${toVerticeName}/${toID}`);
+                    }
+                    continue;
+                  }
+                  await db.createEdge(edgeCollectionName, null,
+                    `${fromVerticeName}/${from_id}`, `${toVerticeName}/${modified_to_idValues}`);
+                }
+              }
+            }
+          }
+          await db.update(collectionName, { id: message.id }, _.omitBy(message, _.isNil));
+        } catch (err) {
+          if (done) {
+            done();
+          }
+        }
+        if (done) {
+          done();
+        }
+        return {};
+      },
+      [`${resource}Deleted`]: async function restoreDeleted(message: any,
+        context: any, config: any, eventName: string, done: any): Promise<any> {
+        try {
+          if (that.edgeCfg[collectionName]) {
+            // Modify the Ids to include documentHandle
+            await db.removeVertex(collectionName, `${collectionName}/${message.id}`);
+          } else {
+            await db.delete(collectionName, { id: message.id });
+          }
+        } catch (err) {
+          if (done) {
+            done();
+          }
+        }
+        if (done) {
+          done();
+        }
+        return {};
+      }
+    };
   }
 }
 
