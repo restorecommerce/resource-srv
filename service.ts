f0ae5cd04d67b4595b776c447928c59ecfe93334
respinha <rui.espinha.ribeiro@n-fuse.co>
2018-07-24 17:12:56 +0200

    Removed try/catch from restore, errors should be caught on chassis

--- service.ts
+++ service.ts
@@ -86,8 +86,8 @@ export class Worker {
 
     const logger = new Logger(cfg.get('logger'));
     const server = new Server(cfg.get('server'), logger);
-    const db = await co(database.get(cfg.get('database:arango'),
-      logger, cfg.get('graph:graphName')));
+    const db = await database.get(cfg.get('database:arango'),
+      logger, cfg.get('graph:graphName'));
     const events = new Events(cfg.get('events:kafka'), logger);
 
     await events.start();
@@ -161,7 +161,7 @@ export class Worker {
       try {
         await cis.command(msg, context);
       } catch (err) {
-        that.logger.error(err);
+        that.logger.error('Error while executing command', err);
       }
     };
 
@@ -231,42 +231,38 @@ class ResourceCommandInterface extends CommandInterface {
       [`${resource}Created`]: async function restoreCreated(message: any,
         context: any, config: any, eventName: string): Promise<any> {
         that.decodeBufferField(message, resource);
-        try {
-          if (that.edgeCfg[collectionName]) {
-            const result = await db.findByID(collectionName, message.id);
-            if (result.length > 0) {
-              return {};
-            }
-            await db.createVertex(collectionName, message);
-            // Based on graphCfg create the necessary edges
-            for (let eachEdgeCfg of that.edgeCfg[collectionName]) {
-              const fromIDkey = eachEdgeCfg.from;
-              const from_id = message[fromIDkey];
-              const toIDkey = eachEdgeCfg.to;
-              const to_id = message[toIDkey];
-              const fromVerticeName = collectionName;
-              const toVerticeName = eachEdgeCfg.toVerticeName;
-              if (fromVerticeName && toVerticeName) {
-                const edgeDefRes = await db.addEdgeDefinition(eachEdgeCfg.edgeName, [fromVerticeName],
-                  [toVerticeName]);
-              }
-              if (from_id && to_id) {
-                if (_.isArray(to_id)) {
-                  for (let toID of to_id) {
-                    await db.createEdge(eachEdgeCfg.edgeName, null,
-                      `${fromVerticeName}/${from_id}`, `${toVerticeName}/${toID}`);
-                  }
-                  continue;
-                }
-                await db.createEdge(eachEdgeCfg.edgeName, null,
-                  `${fromVerticeName}/${from_id}`, `${toVerticeName}/${to_id}`);
-              }
-            }
-          } else {
-            await db.insert(collectionName, message);
+        if (that.edgeCfg[collectionName]) {
+          const result = await db.findByID(collectionName, message.id);
+          if (result.length > 0) {
+            return {};
           }
-        } catch (err) {
-          that.logger.error(`Exception caught restoring ${resource}Created message`, message);
+          await db.createVertex(collectionName, message);
+          // Based on graphCfg create the necessary edges
+          for (let eachEdgeCfg of that.edgeCfg[collectionName]) {
+            const fromIDkey = eachEdgeCfg.from;
+            const from_id = message[fromIDkey];
+            const toIDkey = eachEdgeCfg.to;
+            const to_id = message[toIDkey];
+            const fromVerticeName = collectionName;
+            const toVerticeName = eachEdgeCfg.toVerticeName;
+            if (fromVerticeName && toVerticeName) {
+              const edgeDefRes = await db.addEdgeDefinition(eachEdgeCfg.edgeName, [fromVerticeName],
+                [toVerticeName]);
+            }
+            if (from_id && to_id) {
+              if (_.isArray(to_id)) {
+                for (let toID of to_id) {
+                  await db.createEdge(eachEdgeCfg.edgeName, null,
+                    `${fromVerticeName}/${from_id}`, `${toVerticeName}/${toID}`);
+                }
+                continue;
+              }
+              await db.createEdge(eachEdgeCfg.edgeName, null,
+                `${fromVerticeName}/${from_id}`, `${toVerticeName}/${to_id}`);
+            }
+          }
+        } else {
+          await db.insert(collectionName, message);
         }
         return {};
       },
@@ -274,64 +270,56 @@ class ResourceCommandInterface extends CommandInterface {
         context: any, config: any, eventName: string): Promise<any> {
         that.decodeBufferField(message, resource);
         // Based on graphcfg update necessary edges
-        try {
-          if (that.edgeCfg[collectionName]) {
-            const foundDocs = await db.find(collectionName, { id: message.id });
-            const dbDoc = foundDocs[0];
-            for (let eachEdgeCfg of that.edgeCfg[collectionName]) {
-              const toIDkey = eachEdgeCfg.to;
-              let modified_to_idValues = message[toIDkey];
-              let db_to_idValues = dbDoc[toIDkey];
-              if (_.isArray(modified_to_idValues)) {
-                modified_to_idValues = _.sortBy(modified_to_idValues);
-              }
-              if (_.isArray(db_to_idValues)) {
-                db_to_idValues = _.sortBy(db_to_idValues);
-              }
-              // delete and recreate only if there is a difference in references
-              if (!_.isEqual(modified_to_idValues, db_to_idValues)) {
-                const fromIDkey = eachEdgeCfg.from;
-                const from_id = message[fromIDkey];
-                const fromVerticeName = collectionName;
-                const toVerticeName = eachEdgeCfg.toVerticeName;
+        if (that.edgeCfg[collectionName]) {
+          const foundDocs = await db.find(collectionName, { id: message.id });
+          const dbDoc = foundDocs[0];
+          for (let eachEdgeCfg of that.edgeCfg[collectionName]) {
+            const toIDkey = eachEdgeCfg.to;
+            let modified_to_idValues = message[toIDkey];
+            let db_to_idValues = dbDoc[toIDkey];
+            if (_.isArray(modified_to_idValues)) {
+              modified_to_idValues = _.sortBy(modified_to_idValues);
+            }
+            if (_.isArray(db_to_idValues)) {
+              db_to_idValues = _.sortBy(db_to_idValues);
+            }
+            // delete and recreate only if there is a difference in references
+            if (!_.isEqual(modified_to_idValues, db_to_idValues)) {
+              const fromIDkey = eachEdgeCfg.from;
+              const from_id = message[fromIDkey];
+              const fromVerticeName = collectionName;
+              const toVerticeName = eachEdgeCfg.toVerticeName;
 
-                const edgeCollectionName = eachEdgeCfg.edgeName;
-                let outgoingEdges: any = await db.getOutEdges(edgeCollectionName, `${collectionName}/${dbDoc.id}`);
-                for (let outgoingEdge of outgoingEdges) {
-                  const removedEdge = await db.removeEdge(edgeCollectionName, outgoingEdge._id);
-                }
-                // Create new edges
-                if (from_id && modified_to_idValues) {
-                  if (_.isArray(modified_to_idValues)) {
-                    for (let toID of modified_to_idValues) {
-                      await db.createEdge(eachEdgeCfg.edgeName, null,
-                        `${fromVerticeName}/${from_id}`, `${toVerticeName}/${toID}`);
-                    }
-                    continue;
+              const edgeCollectionName = eachEdgeCfg.edgeName;
+              let outgoingEdges: any = await db.getOutEdges(edgeCollectionName, `${collectionName}/${dbDoc.id}`);
+              for (let outgoingEdge of outgoingEdges) {
+                const removedEdge = await db.removeEdge(edgeCollectionName, outgoingEdge._id);
+              }
+              // Create new edges
+              if (from_id && modified_to_idValues) {
+                if (_.isArray(modified_to_idValues)) {
+                  for (let toID of modified_to_idValues) {
+                    await db.createEdge(eachEdgeCfg.edgeName, null,
+                      `${fromVerticeName}/${from_id}`, `${toVerticeName}/${toID}`);
                   }
-                  await db.createEdge(edgeCollectionName, null,
-                    `${fromVerticeName}/${from_id}`, `${toVerticeName}/${modified_to_idValues}`);
+                  continue;
                 }
+                await db.createEdge(edgeCollectionName, null,
+                  `${fromVerticeName}/${from_id}`, `${toVerticeName}/${modified_to_idValues}`);
               }
             }
           }
-          await db.update(collectionName, { id: message.id }, _.omitBy(message, _.isNil));
-        } catch (err) {
-          that.logger.error(`Exception caught restoring ${resource}Modified message`, message);
         }
+        await db.update(collectionName, { id: message.id }, _.omitBy(message, _.isNil));
         return {};
       },
       [`${resource}Deleted`]: async function restoreDeleted(message: any,
         context: any, config: any, eventName: string): Promise<any> {
-        try {
-          if (that.edgeCfg[collectionName]) {
-            // Modify the Ids to include documentHandle
-            await db.removeVertex(collectionName, `${collectionName}/${message.id}`);
-          } else {
-            await db.delete(collectionName, { id: message.id });
-          }
-        } catch (err) {
-          that.logger.error(`Exception caught restoring ${resource}Deleted message`, message);
+        if (that.edgeCfg[collectionName]) {
+          // Modify the Ids to include documentHandle
+          await db.removeVertex(collectionName, `${collectionName}/${message.id}`);
+        } else {
+          await db.delete(collectionName, { id: message.id });
         }
         return {};
       }
