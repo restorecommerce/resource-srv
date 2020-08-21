10fb9360575b5955ff2c9c04d177ff6277936d73
akumar <arun.kumar@n-fuse.co>
2020-08-21 12:14:58 +0200

    export worker after restructuring so that it can be used as wrapper by another service

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,8 @@
 'use strict';
 
 import * as Cluster from '@restorecommerce/cluster-service';
+import { Worker } from './worker';
+export { Worker };
 
 const cfg = require('@restorecommerce/service-config')(process.cwd());
 const server = new Cluster(cfg);
